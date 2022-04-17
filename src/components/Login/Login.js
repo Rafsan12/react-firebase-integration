49d94b8c5aef0bccdcd5803bc0318fd4ce81c059
Rafsan12 <rafsan.web.2022@gmail.com>
2022-04-17 06:13:50 +0600

    create-hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {singInWithGoogle} = useFirebase();
     return (
         <div>
             <h3>please log in</h3>
+            <div style={{margin:'20px'}}>
+               <button onClick={singInWithGoogle}>Google Sing In</button> 
+            </div>
             <form>
             <input type="text" placeholder='Your Name' />
             <br />
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,26 @@
+import {  useState } from "react"
+import app from "../firebase.init";
+import {getAuth , GoogleAuthProvider, signInWithPopup , } from 'firebase/auth';
+
+
+const auth =getAuth(app)
+const googleProvider = new GoogleAuthProvider()
+
+
+const useFirebase = () =>{
+    const [user ,setUser] = useState({});
+   
+    const singInWithGoogle =() =>{
+       signInWithPopup (auth , googleProvider)
+       .then(result =>{
+           const user = result.user;
+           setUser(user)
+
+       })
+    }
+    
+    return { user , singInWithGoogle}
+}
+
+
+export default useFirebase;
